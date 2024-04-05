d85cf9002ac0cca02eeb7ca22a2a4a11b08cc86e
Chris-Mwesigwa <cmwesigwa@aetotechs.com>
2024-04-05 15:29:52 +0300

    items name display home page

--- src/main-pages/home/DHome.jsx
+++ src/main-pages/home/DHome.jsx
@@ -143,11 +143,11 @@ const DHome = () => {
 
                     return(
                 <div key={index} className="i-card">
-                    <Link to={'/details/' +item?.itemID} className="i-card-image" onClick={() => navigate('/details/' + item.itemID)}>
+                    <Link title={item?.name} to={'/details/' +item?.itemID} className="i-card-image" onClick={() => navigate('/details/' + item.itemID)}>
                         <img src={item?.coverPhoto?.url} width={'100%'} height={'100%'} style={{objectFit: 'contain', borderRadius: 4}}/>
                     </Link>
                     <Link to={'/details/' +item?.itemID} className="i-info-section">
-                        <p className="i-name">{item?.name}</p>
+                        <p className="i-name" title={item?.name}>{item?.name}</p>
                         <div className="i-price">
                             <p style={{fontSize: 10, color: 'grey'}}>UGX <span style={{fontSize: 16, color: 'black'}}> {(item?.price).toLocaleString()}</span></p> 
                             <div style={{ color: 'rgba(0,0,0,0.4)', fontSize: 12, textDecoration: 'line-through', fontWeight: 'normal'}}>UGX {(item?.globalPrice).toLocaleString()}</div>
